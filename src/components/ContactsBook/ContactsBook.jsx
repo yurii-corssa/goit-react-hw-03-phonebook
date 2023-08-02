b3433444d813f76ad029125672ca56a08b58cb1c
yurii-corssa <yurii.corssa@gmail.com>
2023-08-02 19:15:43 +0200

    save contacts to lockal storage

--- src/components/ContactsBook/ContactsBook.jsx
+++ src/components/ContactsBook/ContactsBook.jsx
@@ -13,14 +13,20 @@ import { Filter } from './Filter/Filter';
 
 export class ContactsBook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsLS = JSON.parse(localStorage.getItem('contacts')) || [];
+    this.setState({ contacts: contactsLS });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   submitForm = data => {
     if (this.findContact(data.name)) {
